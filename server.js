fcdfb078482df0953990b8d7480e2a6260495301
Rutuja Suryawanshi <suryawanshirutuja94@gmail.com>
2026-03-12 19:41:13 +0530

    Add WebRTC signaling and video grid support

--- server.js
+++ server.js
@@ -1,34 +1,50 @@
-const express = require("express")
-const app = express()
-const server = require("http").createServer(app)
-const io = require("socket.io")(server)
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
-app.use(express.static("."))
+app.use(express.static("."));
 
 io.on("connection", socket => {
+  console.log("User connected:", socket.id);
 
-console.log("User connected")
+  socket.on("join-room", roomId => {
+    socket.join(roomId);
+    socket.roomId = roomId;
+    // Notify others in the room
+    socket.to(roomId).emit("user-joined", socket.id);
+    console.log(`User ${socket.id} joined room ${roomId}`);
+  });
 
-socket.on("join-room", roomId => {
+  // Chat messages
+  socket.on("chat-message", message => {
+    io.to(socket.roomId).emit("chat-message", message);
+  });
 
-socket.join(roomId)
+  // WebRTC signaling: offer
+  socket.on("offer", ({ offer, to }) => {
+    io.to(to).emit("offer", { offer, from: socket.id });
+  });
 
-socket.roomId = roomId
+  // WebRTC signaling: answer
+  socket.on("answer", ({ answer, to }) => {
+    io.to(to).emit("answer", { answer, from: socket.id });
+  });
 
-io.to(roomId).emit("user-joined", socket.id)
+  // WebRTC signaling: ICE candidates
+  socket.on("candidate", ({ candidate, to }) => {
+    io.to(to).emit("candidate", { candidate, from: socket.id });
+  });
 
-})
-
-socket.on("chat-message", message => {
-
-io.to(socket.roomId).emit("chat-message", message)
-
-})
-
-})
+  // Handle disconnect
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+    if (socket.roomId) {
+      socket.to(socket.roomId).emit("user-left", socket.id);
+    }
+  });
+});
 
 server.listen(3000, () => {
-
-console.log("Server running on http://localhost:3000")
-
-})
+  console.log("Server running on http://localhost:3000");
+});
